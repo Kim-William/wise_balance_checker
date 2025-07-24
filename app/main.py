78644f7b992704a897b71d2aead9e7e7b8513ccf
WoongKeol Kim <130305118+Kim-William@users.noreply.github.com>
2025-07-24 14:19:29 +0100

    ignore pycache

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 # main.py
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request 
 from app.api.v1.router import router
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
-app = FastAPI(
-    title="Wise Balance Checker"
-)
+app = FastAPI(title="Wise Balance Checker")
+templates = Jinja2Templates(directory="app/templates")
+
+@app.get("/dashboard", response_class=HTMLResponse)
+async def show_dashboard(request: Request):
+    return templates.TemplateResponse("dashboard.html", {"request": request})
 
 # Register API router
-print('asdasd')
 app.include_router(router, prefix="/api/v1")
